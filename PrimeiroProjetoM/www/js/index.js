121b2dc17af1f760dccb9f61be4c84534eea9838
RICH <richard_inada@hotmail.com>
2021-09-28 20:19:13 -0300

    edicao do index js

--- PrimeiroProjetoM/www/js/index.js
+++ PrimeiroProjetoM/www/js/index.js
@@ -21,8 +21,71 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+let db;
+
+function entrar(){
+    console.log("function entrar(){...");
+    console.log(db);
+
+    let login = document.getElementById("loginSen").value;
+    let pass = document.getElementById("loginSen").value;
+
+        db.transaction(function(tx) {
+            tx.executeSql('INSERT INTO usuarios VALUES (?,?)', [login, pass]);
+        }, function(error) {
+            alert('Erro durante a transacao com o banco: ' + error.message);
+        }, function() {
+            alert('Insercao realizada com sucesso!!!');
+        });
+    }
+}
+
+function criarconta(){
+    console.log("function criar(){...");
+    console.log(db);
+
+    db.executeSql(
+        'SELECT login AS uLoginName, pass AS uPassword FROM usuarios', [], function(rs) {
+            alert(JSON.stringify(rs));
+            alert(rs.rows.length);
+            let i = 0;
+            for(i = 0; i < rs.rows.length; i++){
+                alert("item "+i);
+                let recordItem = rs.rows.item(i);
+                alert(JSON.stringify(recordItem));
+            }                
+    }, function(error) {
+        alert('Erro no SELECT: ' + error.message);
+    });
+}
+
+
+function inicializarbanco(){
+    console.log("function inicializarbanco(){...")
+    db = window.sqlitePlugin.openDatabase({
+        name: "my db",
+        location: "default",
+        androidDatabaseProvider: "system"
+    });
+
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (login, pass)');
+    }, function(error) {
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        alert('Banco e Tabela usuarios criados com sucesso!!!');
+    });
+}
+
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
+
+    inicializarbanco();
+
+    document.getElementById("loginBut").addEventListener("click", app.entrar);
+    document.getElementById("loginBut-criar").addEventListener("click", app.criarconta);
+    this.receivedEvent('deviceready');
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
